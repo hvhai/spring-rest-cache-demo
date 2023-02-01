7c529d89600d503fd5219f8e82fa96de8714237d
hvhai <hunterx6323@gmail.com>
2023-02-01 15:35:09 +0700

    init project

---
+++ src/main/java/com/codehunter/springrestcachedemo/SpringRestCacheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codehunter.springrestcachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCacheDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehunter/springrestcachedemo/SpringRestCacheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehunter.springrestcachedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCacheDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
